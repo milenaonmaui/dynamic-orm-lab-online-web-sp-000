08bd2e2a289ecd4dc2f094aaad82966744499152
Milena Kari <milenakari@yahoo.com>
2019-01-25 04:22:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,4 +53,8 @@ def self.table_name
     DB[:conn].execute(sql)
   end
 
+  def self.find_by_name(name)
+     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+     DB[:conn].execute(sql)
+   end
 end
